913f44f1495bbe96aad614a4206cdeef49128e4f
Florian Rheinschmidt <rheinschmidt@ismd.tu-darmstadt.de>
2025-04-24 18:32:05 +0200

    Implement mode III into Layered class
    - Add thickness and rotation as argument

--- weac/layered.py
+++ weac/layered.py
@@ -30,35 +30,26 @@ class Layered(
     analysis from AnalysisMixin().
     """
 
-    def __init__(self, system="pst-", layers=None, touchdown=False):
+    def __init__(self, system='pst-', layers=None, phi=0, theta = 0, b = 290, touchdown = False):
         """
         Initialize model with user input.
 
         Arguments
         ---------
-        system : {'pst-', '-pst', 'vpst-', '-vpst', 'skier', 'skiers'}, optional
+        system : {'pst-', '-pst', 'skier', 'skiers'}, optional
             Type of system to analyse: PST cut from the right (pst-),
-            PST cut form the left (-pst), PST with vertical faces cut
-            from the right (vpst-), PST with vertical faces cut from the
-            left (-vpst), one skier on infinite slab (skier) or multiple
-            skiers on infinite slab (skiers). Default is 'pst-'.
+            PST cut form the left (-pst), one skier on infinite
+            slab (skier) or multiple skiers on infinite slab (skiers).
+            Default is 'pst-'.
         layers : list, optional
             2D list of layer densities and thicknesses. Columns are
             density(kg/m ^ 3) and thickness(mm). One row corresponds
             to one layer. Default is [[240, 200], ].
-        touchdown : bool, optional
-            Set True if slab touchdown is to be considered. Default is False.
         """
         # Call parent __init__
-        super().__init__(system=system, touchdown=touchdown)
+        super().__init__(system=system)
 
         # Set material properties and set up model
-        self.set_beam_properties(
-            layers
-            if layers
-            else [
-                [240, 200],
-            ]
-        )
+        self.set_beam_properties(layers if layers else [[240, 200], ], phi = phi, theta=theta, b=b)
         self.set_foundation_properties()
         self.calc_fundamental_system()
