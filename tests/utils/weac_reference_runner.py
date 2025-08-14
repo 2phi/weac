9065998d0b980376a08567e0a5bf5404d04b5f38
Philipp Rosendahl <zacc@live.de>
2025-08-14 13:20:48 +0200

    chore: Update default WEAC reference version to 2.6.4 in weac_reference_runner.py

--- tests/utils/weac_reference_runner.py
+++ tests/utils/weac_reference_runner.py
@@ -32,7 +32,7 @@ except ImportError:
         _np = Any  # type: ignore[assignment, misc]
 
 
-DEFAULT_REFERENCE_VERSION = os.environ.get("WEAC_REFERENCE_VERSION", "2.6.3")
+DEFAULT_REFERENCE_VERSION = os.environ.get("WEAC_REFERENCE_VERSION", "2.6.4")
 REFERENCE_HOME = os.environ.get("WEAC_REFERENCE_HOME", None)
 
 
