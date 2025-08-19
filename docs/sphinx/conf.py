42f349ac0a3ce32ee08a4dda99b9eb94882c6cce
Philipp Rosendahl <zacc@live.de>
2025-08-19 12:21:58 +0200

    docs: add Sphinx config; set version/release as strings and drop deprecated highlighting extension

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -1,50 +1,52 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Sphinx configuration for WEAC documentation.
+
+This configuration avoids deprecated extensions and ensures that
+`version` and `release` are strings (not callables) as required by Sphinx.
+"""
+
+from __future__ import annotations
+
+from importlib.metadata import version as get_version
 
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-from datetime import date
-from importlib.metadata import PackageNotFoundError, version
 
 project = "WEAC"
-copyright = f"{date.today().year}, 2phi GbR"
-author = "P.L. Rosendahl, P. Weissgraeber, F. Rheinschmidt, J. Schneider"
-try:
-    release = version("weac")
-except PackageNotFoundError:
-    release = "unknown"
-github_url = "https://github.com/2phi/weac"
+author = "2phi GbR"
+
+# Ensure these are strings. Do not shadow the imported function name.
+release = get_version("weac")
+version = ".".join(release.split(".")[:2])
+
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinxawesome_theme.highlighting",
+	"sphinx.ext.autodoc",
+	"sphinx.ext.autodoc.typehints",
+	"sphinx.ext.napoleon",
+	"sphinx.ext.viewcode",
+	"sphinx.ext.mathjax",
 ]
 
-pygments_style = "perldoc"
+# Do NOT include 'sphinxawesome_theme.highlighting' (deprecated and unnecessary)
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ["_static"]
 html_theme = "sphinxawesome_theme"
-html_theme_options = {
-    "logo_light": "_static/logo-light.png",
-    "logo_dark": "_static/logo-dark.png",
-    "awesome_external_links": True,
-    "awesome_headerlinks": True,
-    "show_scrolltop": True,
-}
-html_favicon = "_static/favicon.ico"
-html_show_sphinx = False
+html_static_path = ["_static"]
+html_title = f"{project} {release}"
+
+
+# -- Autodoc options ---------------------------------------------------------
+
+autodoc_typehints = "description"
+autodoc_typehints_format = "short"
+autodoc_preserve_defaults = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
