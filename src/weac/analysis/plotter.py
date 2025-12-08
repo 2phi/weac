Fix: Normalization plot_deformed

src/weac/analysis/plotter.py
@@ -908,21 +908,24 @@ class Plotter:
                 slab = 1e4 * Wsl
                 weak = 1e4 * Wsl[-1, nanmask]
                 label = r"$w$ ($\mu$m)"
-            # Axial normal stresses (kPa)
             case "Sxx":
-                slab = analyzer.Sxx(z, phi, dz=dz, unit="kPa")
+                slab = analyzer.Sxx(z, phi, dz=dz, unit="kPa", normalize=normalize)
                 weak = np.zeros(xwl_finite.shape[0])
-                label = r"$\sigma_{xx}$ (kPa)"
+                label = (
+                    r"$\sigma_{xx}/\sigma_+$" if normalize else r"$\sigma_{xx}$ (kPa)"
+                )
             # Shear stresses (kPa)
             case "Txz":
-                slab = analyzer.Txz(z, phi, dz=dz, unit="kPa")
+                slab = analyzer.Txz(z, phi, dz=dz, unit="kPa", normalize=normalize)
                 weak = Tauwl[nanmask]
-                label = r"$\tau_{xz}$ (kPa)"
+                label = r"$\tau_{xz}/\sigma_+$" if normalize else r"$\tau_{xz}$ (kPa)"
             # Transverse normal stresses (kPa)
             case "Szz":
-                slab = analyzer.Szz(z, phi, dz=dz, unit="kPa")
+                slab = analyzer.Szz(z, phi, dz=dz, unit="kPa", normalize=normalize)
                 weak = Sigmawl[nanmask]
-                label = r"$\sigma_{zz}$ (kPa)"
+                label = (
+                    r"$\sigma_{zz}/\sigma_+$" if normalize else r"$\sigma_{zz}$ (kPa)"
+                )
             # Principal stresses
             case "principal":
                 slab = analyzer.principal_stress_slab(
@@ -942,11 +945,6 @@ class Plotter:
                         r"$\sigma_\mathrm{I}$ (kPa, slab),  "
                         r"$\sigma_\mathrm{I\!I\!I}$ (kPa, weak layer)"
                     )
-            case _:
-                raise ValueError(
-                    f"Invalid input '{field}' for field. Valid options are "
-                    "'u', 'w', 'Sxx', 'Txz', 'Szz', or 'principal'"
-                )
 
         # Complement label
         label += r"  $\longrightarrow$"