dbdbc09815fbc74eb5700c6e0aa957a771ae18bf
Flo-rhein <florian.rheinschmidt@uni-rostock.de>
2026-02-17 14:20:31 +0100

    Enhance Segment class: add is_loaded attribute to distinguish if the segment is subject to additional loading or not

--- src/weac/components/segment.py
+++ src/weac/components/segment.py
@@ -2,8 +2,10 @@
 This module defines the Segment class, which represents a segment of the snowpack.
 """
 
-from pydantic import BaseModel, Field
-
+import numpy as np
+from pydantic import BaseModel, Field, field_validator
+from pydantic import ConfigDict
+from numpy.typing import NDArray
 
 class Segment(BaseModel):
     """
@@ -16,16 +18,34 @@ class Segment(BaseModel):
     has_foundation: bool
         Whether the segment is supported (foundation present) or cracked/free-hanging
         (no foundation).
+    is_loaded: bool
+        Whether additional loading is applied at the segment's top side.
     m: float
         Skier mass at the segment's right edge [kg].
     """
-
+    model_config = ConfigDict(arbitrary_types_allowed=True)
     length: float = Field(default=5e3, ge=0, description="Segment length in [mm]")
     has_foundation: bool = Field(
         default=True,
         description="Whether the segment is supported (foundation present) or "
         "cracked/free-hanging (no foundation)",
     )
+    is_loaded: bool = Field(
+        default=True, description="Whether additional loading is applied at the segment's top side"
+    )
     m: float = Field(
         default=0, ge=0, description="Skier mass at the segment's right edge in [kg]"
     )
+
+    f: np.ndarray = Field(
+        default_factory=lambda: np.zeros((6,1), dtype=float),
+        description=(
+            "Load vector acting on the right side of the segment. "
+            "Includes the six section forces [Nx, Vy, Vz, Mx, My, Mz]."
+        ),
+    )
+
+    @field_validator("f")
+    def ensure_ndarray(cls, v: NDArray[np.float64]) -> NDArray[np.float64]:
+        # allows passing lists/tuples etc. and enforces float dtype
+        return np.asarray(v, dtype=float)
