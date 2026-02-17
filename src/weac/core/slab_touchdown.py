c43d02ed917995e34352e4e0d16d5e63d5b61bea
Flo-rhein <florian.rheinschmidt@uni-rostock.de>
2026-02-17 14:21:40 +0100

    fix: update variable reference from qn to qz in SlabTouchdown class for correct scenario loading

--- src/weac/core/slab_touchdown.py
+++ src/weac/core/slab_touchdown.py
@@ -147,7 +147,7 @@ class SlabTouchdown:  # pylint: disable=too-many-instance-attributes,too-few-pub
         ss = self.eigensystem.kA55
         L = self.scenario.L
         crack_h = self.scenario.crack_h
-        qn = self.scenario.qn
+        qn = self.scenario.qz
 
         # Create polynomial expression
         def polynomial(x: float) -> float:
@@ -185,7 +185,7 @@ class SlabTouchdown:  # pylint: disable=too-many-instance-attributes,too-few-pub
         ss = self.eigensystem.kA55
         L = self.scenario.L
         crack_h = self.scenario.crack_h
-        qn = self.scenario.qn
+        qn = self.scenario.qz
 
         # Create polynomial function
         def polynomial(x: float) -> float:
@@ -240,7 +240,7 @@ class SlabTouchdown:  # pylint: disable=too-many-instance-attributes,too-few-pub
         L = self.scenario.L
         cut_length = self.scenario.cut_length
         crack_h = self.scenario.crack_h
-        qn = self.scenario.qn
+        qn = self.scenario.qz
 
         # Spring stiffness of uncollapsed eigensystem of length L - cut_length
         straight_scenario = self._generate_straight_scenario(L - cut_length)
